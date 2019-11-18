bf2c321fc3af3faca687fff9533450b8107804c2
Igor Stepin <igor@stepin.name>
2019-11-19 03:20:38 +0400

    Fix class name

--- src/main/java/name/stepin/Application.java
+++ src/main/java/name/stepin/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTemplateAppApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringTemplateAppApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
