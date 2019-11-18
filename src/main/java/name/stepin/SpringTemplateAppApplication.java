33fb5990c09c030e2ac85dccfca979a1f227aefe
Igor Stepin <igor@stepin.name>
2019-11-19 03:08:36 +0400

    Initial from start.spring.io

---
+++ src/main/java/name/stepin/SpringTemplateAppApplication.java
@@ -0,0 +1,13 @@
+package name.stepin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemplateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTemplateAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/name/stepin/SpringTemplateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package name.stepin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTemplateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
