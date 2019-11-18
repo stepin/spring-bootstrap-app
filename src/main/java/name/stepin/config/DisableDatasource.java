f9f7fbaf1ea944c77754cc272936268fbe5a18f8
Igor Stepin <igor@stepin.name>
2019-11-19 03:49:05 +0400

    Fixed build

---
+++ src/main/java/name/stepin/config/DisableDatasource.java
@@ -0,0 +1,11 @@
+package name.stepin.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class DisableDatasource {
+
+}
