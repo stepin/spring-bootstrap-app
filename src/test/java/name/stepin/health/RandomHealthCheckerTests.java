6ed91bbb96e212594ea52e75f731bf17d0f0ae5f
Igor Stepin <igor@stepin.name>
2019-12-01 12:58:01 +0400

    added PowerMock

---
+++ src/test/java/name/stepin/health/RandomHealthCheckerTests.java
@@ -0,0 +1,45 @@
+package name.stepin.health;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Health.Builder;
+import org.springframework.boot.actuate.health.Status;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({RandomHealthChecker.class, Math.class})
+public class RandomHealthCheckerTests {
+
+  @Test
+  void healthIsUp() {
+    PowerMockito.mockStatic(Math.class);
+    PowerMockito.when(Math.random()).thenReturn(0.6);
+
+    RandomHealthChecker checker = new RandomHealthChecker();
+    Health.Builder builder = new Builder();
+
+    checker.doHealthCheck(builder);
+
+    Health health = builder.build();
+    assertThat(health.getStatus()).isEqualTo(Status.UP);
+  }
+
+  @Test
+  void healthIsDown() {
+    PowerMockito.mockStatic(Math.class);
+    PowerMockito.when(Math.random()).thenReturn(0.1);
+
+    RandomHealthChecker checker = new RandomHealthChecker();
+    Health.Builder builder = new Builder();
+
+    checker.doHealthCheck(builder);
+
+    Health health = builder.build();
+    assertThat(health.getStatus()).isEqualTo(Status.DOWN);
+  }
+}
