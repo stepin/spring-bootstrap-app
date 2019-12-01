476246c1efc3a601fb18b55c837d5046f657f3e7
Igor Stepin <igor@stepin.name>
2019-12-01 13:51:45 +0400

    Better API documentation

---
+++ README.md
@@ -0,0 +1,11 @@
+Spring Template App
+===================
+
+Links
+-----
+
+1. http://localhost:8080/swagger-ui.html
+1. http://localhost:8080/v2/api-docs
+1. http://localhost:8080/example
+1. http://localhost:8080/example/app_config
+1. http://localhost:8080/example/git_branch
---
+++ src/main/java/name/stepin/config/SwaggerConfig.java
@@ -0,0 +1,81 @@
+package name.stepin.config;
+
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.ModelRendering;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger.web.TagsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger configuration for Spring MVC classes.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+  @Bean
+  SecurityConfiguration security() {
+    return SecurityConfigurationBuilder.builder()
+        .clientId("test-app-client-id")
+        .clientSecret("test-app-client-secret")
+        .realm("test-app-realm")
+        .appName("test-app")
+        .scopeSeparator(",")
+        .additionalQueryStringParams(null)
+        .useBasicAuthenticationWithAccessCodeGrant(false)
+        .build();
+  }
+
+  @Bean
+  UiConfiguration uiConfig() {
+    return UiConfigurationBuilder.builder()
+        .deepLinking(true)
+        .displayOperationId(false)
+        .defaultModelsExpandDepth(1)
+        .defaultModelExpandDepth(1)
+        .defaultModelRendering(ModelRendering.EXAMPLE)
+        .displayRequestDuration(false)
+        .docExpansion(DocExpansion.NONE)
+        .filter(false)
+        .maxDisplayedTags(null)
+        .operationsSorter(OperationsSorter.ALPHA)
+        .showExtensions(false)
+        .tagsSorter(TagsSorter.ALPHA)
+        .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+        .validatorUrl(null)
+        .build();
+  }
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(new ApiInfo(
+            "API Documentation",
+            "API Documentation",
+            "0.1",
+            null,
+            new Contact("Igor Stepin", null, null),
+            null,
+            null,
+            Collections.emptyList())
+        );
+  }
+
+}
