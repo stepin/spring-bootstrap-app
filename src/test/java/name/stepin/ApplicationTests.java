ad3a73b450ce1629a6389c1c70ba74749f7b643a
Igor Stepin <igor@stepin.name>
2019-12-02 11:51:43 +0400

    Fix tests with Graphql

--- src/test/java/name/stepin/ApplicationTests.java
+++ src/test/java/name/stepin/ApplicationTests.java
@@ -3,7 +3,7 @@ package name.stepin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
 	@Test
