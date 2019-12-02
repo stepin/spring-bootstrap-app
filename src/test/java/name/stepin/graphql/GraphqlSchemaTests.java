6031f812488f2793dcd269b80a1858d551c582be
Igor Stepin <igor@stepin.name>
2019-12-02 13:03:19 +0400

    Refactor

--- src/test/java/name/stepin/graphql/GraphqlSchemaTests.java
+++ src/test/java/name/stepin/graphql/GraphqlSchemaTests.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import com.graphql.spring.boot.test.GraphQLResponse;
 import com.graphql.spring.boot.test.GraphQLTest;
 import com.graphql.spring.boot.test.GraphQLTestTemplate;
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,24 +19,25 @@ class GraphqlSchemaTests {
 
   @Test
   void getExample() throws Exception {
-    // When
-    GraphQLResponse response = graphQLTestTemplate.postForResource("/graphql/query-example.graphql");
+    GraphQLResponse response = successfulGraphQLPost("query-example.graphql");
 
-    // Then
-    assertThat(response).isNotNull();
-    assertThat(response.isOk()).isTrue();
     assertThat(response.get("$.data.example")).isNotBlank();
   }
 
   @Test
   void createExample() throws Exception {
-    // When
-    GraphQLResponse response = graphQLTestTemplate.postForResource("/graphql/mutation-create-example.graphql");
+    GraphQLResponse response = successfulGraphQLPost("mutation-create-example.graphql");
 
-    // Then
+    assertThat(response.get("$.data.createExample")).isNotBlank();
+  }
+
+  private GraphQLResponse successfulGraphQLPost(String resourceName) throws IOException {
+    GraphQLResponse response = graphQLTestTemplate.postForResource("/graphql/" + resourceName);
+
     assertThat(response).isNotNull();
     assertThat(response.isOk()).isTrue();
-    assertThat(response.get("$.data.createExample")).isNotBlank();
+
+    return response;
   }
 
 }
